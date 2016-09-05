d89098f6728cf3005bb5496843b8adffcf5ae146
ddrz <diego.denizard@gmail.com>
2016-09-05 04:24:05 -0300

    Criação de tela de atendente
    Foi criado a tela de atendente, formulario para novo atendente, tambem
    foi criado a respectica controller. Pequenos ajustes na tela de login,
    layout principal com alterações no sidemenu, e remoçao de arquivos
    desnecessarios do projeto.

---
+++ application/controllers/AtendenteController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego Denizard
+ * Date: 04/09/2016
+ */
+
+class AtendenteController extends Zend_Controller_Action
+{
+
+    public function atendenteAction()
+    {
+        // action body
+    }
+
+    public function addAction()
+    {
+        // action body
+    }
+
+}
