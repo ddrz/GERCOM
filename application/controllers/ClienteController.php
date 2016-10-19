c8b62f98979ba057ac0cd1113d8dbb1aae8a3228
mikhailgorba <mikhailgorba@hotmail.com>
2016-10-19 20:30:16 -0200

    Cliente adicionar no formulario

--- application/controllers/ClienteController.php
+++ application/controllers/ClienteController.php
@@ -4,13 +4,41 @@ class ClienteController extends Zend_Controller_Action
 {
 
     public function formularioAction()
-    {
-        // action body
+    {   
+        $modelEntidade = new Application_Model_Entidade();
+        $rowSet = $modelEntidade->fetchAll(); // recupera todos os dados da tabela 'tb_entidade' do banco de dados
+  
+        $dados = $this->_getAllParams(); //recupera os dados enviados pelo formulario do method POST ou GET
+        $modelEntidade = new Application_Model_Entidade();
+        
+        if (!empty($dados['id_entidade'])){
+            
+            $row = $modelEntidade->fetchRow('id_entidade=' .$dados['id_entidade']);
+        
+        } else {
+        
+            $row = $modelEntidade->createRow();
+        }
+        
+        $this->view->row = $row;
     }
 
     public function paginaAction()
     {
 
     }
+    
+     public function gravarAction()
+    {     
+        $dados = $this->_getAllParams(); //recupera os dados enviados pelo formulario
+        
+        $modelEntidade = new Application_Model_Entidade();
+        
+        $modelEntidade->gravar($dados);
+        
+        $this->redirect('cliente/pagina');
+    }
+    
+   
 
 }
