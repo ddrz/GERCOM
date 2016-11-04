ce5ddf1dbd846d8f81c29ac09ac4111ddec01d2b
mikhailgorba <mikhailgorba@hotmail.com>
2016-11-04 18:33:45 -0200

    Update Models da listagem Histórico
    Novas models instanciadas

--- application/controllers/HistoricoController.php
+++ application/controllers/HistoricoController.php
@@ -5,10 +5,18 @@ class HistoricoController extends Zend_Controller_Action
 
     public function historicoAction()
     {
-        $modelHistorico = new Application_Model_Historico();
-        $rowSet = $modelHistorico->fetchAll(); // recupera todos os dados da tabela 'tb_historico' do banco de dados
-
-        $this->view->rowSet = $rowSet;
+      //Entidade
+       $modelEntidade = new Application_Model_Entidade(); //Instanciando a model
+       $rowSetEntidade = $modelEntidade->fetchAll(); //buscando TODA a tabela do banco de dados
+       $this->view->rowSetEntidade = $rowSetEntidade; //enviando pra view
+      //Histórico
+       $modelHistorico = new Application_Model_Historico();
+       $rowSetHistorico = $modelHistorico->fetchAll();
+       $this->view->rowSetHistorico = $rowSetHistorico;
+      //Endereço
+       $modelEndereco = new Application_Model_Endereco();
+       $rowSetEndereco = $modelEndereco->fetchAll();
+       $this->view->rowSetEndereco = $rowSetEndereco;
     }
 
     public function novoAction()
