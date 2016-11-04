8eb807475fca251ad3005984d9f73acb422da06c
mikhailgorba <mikhailgorba@hotmail.com>
2016-11-04 18:31:08 -0200

    Preparando para receber os campos do form HISTÒRICO
    Instanciando as models necessárias para caso de alteração, ou novo
    histórico

--- application/controllers/HistoricoController.php
+++ application/controllers/HistoricoController.php
@@ -6,14 +6,25 @@ class HistoricoController extends Zend_Controller_Action
     public function historicoAction()
     {
         $modelHistorico = new Application_Model_Historico();
-        $rowSet = $modelHistorico->fetchAll(); // recupera todos os dados da tabela 'tb_perfil' do banco de dados
+        $rowSet = $modelHistorico->fetchAll(); // recupera todos os dados da tabela 'tb_historico' do banco de dados
 
         $this->view->rowSet = $rowSet;
     }
 
     public function novoAction()
     {
-
+       //Entidade
+        $modelEntidade = new Application_Model_Entidade(); //Instanciando a model
+        $rowSetEntidade = $modelEntidade->fetchAll(); //buscando TODA a tabela do banco de dados
+        $this->view->rowSetEntidade = $rowSetEntidade; //enviando pra view
+       //Histórico
+        $modelHistorico = new Application_Model_Historico();
+        $rowSetHistorico = $modelHistorico->fetchAll();
+        $this->view->rowSetHistorico = $rowSetHistorico;
+       //Endereço
+        $modelEndereco = new Application_Model_Endereco();
+        $rowSetEndereco = $modelEndereco->fetchAll();
+        $this->view->rowSetEndereco = $rowSetEndereco;
     }
 
     public function gravarAction()
@@ -23,6 +34,7 @@ class HistoricoController extends Zend_Controller_Action
         $modelEntidade = new Application_Model_Entidade();
         $modelEndereco = new Application_Model_Endereco();
 
+        $rowSet = $modelEndereco->fetchAll();
         $modelEndereco->gravar($dados);
         $modelHistorico->gravar($dados);
         $modelEntidade->gravar($dados);
